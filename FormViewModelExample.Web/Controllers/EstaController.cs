d8e7a302940d181fa4aa6214211a9b433def1cbd
Janiek <janiek.colpaert@howest.be>
2023-03-21 14:03:14 +0100

    Add esta form

---
+++ FormViewModelExample.Web/Controllers/EstaController.cs
@@ -0,0 +1,22 @@
+﻿using FormViewModelExample.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FormViewModelExample.Web.Controllers
+{
+    public class EstaController : Controller
+    {
+        public IActionResult Apply()
+        {
+            EstaApplyViewModel model = new EstaApplyViewModel();
+            model.ArrivalDate = DateTime.Now;
+            model.Countries = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Nederland", Value = "NL"},
+                new SelectListItem { Text = "België", Value = "BE"},
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ FormViewModelExample.Web/ViewModels/EstaApplyViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormViewModelExample.Web.ViewModels
+{
+    public class EstaApplyViewModel
+    {
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ArrivalDate { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mailadres")]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [DataType(DataType.PostalCode)]
+        public string PostalCode { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Province { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        public List<SelectListItem> Countries { get; set; }
+
+        public bool ReceiveByMail { get; set; }
+    }
+}
